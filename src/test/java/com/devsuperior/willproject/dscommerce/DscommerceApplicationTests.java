c735d256bb849a567ed9ad0d1ab9c741acd78e8b
William Sousa <williamrsousa94@hotmail.com>
2025-05-26 10:15:00 -0300

    1st commit

---
+++ src/test/java/com/devsuperior/willproject/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.willproject.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
