db7d5eb7ce441ef93318b70fc6b69db783bd167e
William Sousa <williamrsousa94@hotmail.com>
2026-05-20 13:04:23 -0300

    Finalização aula Order,Enum,relacionamento muitos-para-um DevSuperior

--- src/main/java/com/devsuperior/willproject/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/willproject/dscommerce/entities/User.java
@@ -3,6 +3,8 @@ package com.devsuperior.willproject.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +19,9 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){}
 
@@ -75,6 +80,10 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
