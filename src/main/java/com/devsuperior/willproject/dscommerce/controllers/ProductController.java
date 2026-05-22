824f125bcf29473a4748363f2bd7bcc0902ecd7b
William Sousa <williamrsousa94@hotmail.com>
2026-05-22 19:52:46 -0300

    1o teste da API com Repository, retornando um produto DevSuperior

--- src/main/java/com/devsuperior/willproject/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/willproject/dscommerce/controllers/ProductController.java
@@ -1,15 +1,25 @@
 package com.devsuperior.willproject.dscommerce.controllers;
 
+import com.devsuperior.willproject.dscommerce.entities.Product;
+import com.devsuperior.willproject.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository repository;
+
     @GetMapping
     public String teste() {
-        return "Olá, Mundo!";
+        Optional<Product> result = repository.findById(1L);
+        Product product = result.get();
+        return product.getName();
     }
 }
---
+++ src/main/java/com/devsuperior/willproject/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.willproject.dscommerce.repositories;
+
+import com.devsuperior.willproject.dscommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+
+}
