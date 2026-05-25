1efb85615bdf5e104177dacc93549858ad3e3421
William Sousa <williamrsousa94@hotmail.com>
2026-05-25 14:49:16 -0300

    POST mapping DevSuperior

--- src/main/java/com/devsuperior/willproject/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/willproject/dscommerce/services/ProductService.java
@@ -29,4 +29,17 @@ public class ProductService {
         Page<Product> result = productRepository.findAll(pageable);
         return result.map(x -> new ProductDTO(x));
     }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setImgUrl(dto.getImgUrl());
+        entity.setPrice(dto.getPrice());
+
+        entity = productRepository.save(entity);
+        return new ProductDTO(entity);
+    }
 }
