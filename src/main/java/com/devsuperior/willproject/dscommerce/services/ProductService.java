bcfba1a745ee4a19d79b2059040bb5db427b7779
William Sousa <williamrsousa94@hotmail.com>
2026-05-25 20:13:54 -0300

    POST efetuado (criei como treino o PUT também) + ResponseEntity DevSuperior

--- src/main/java/com/devsuperior/willproject/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/willproject/dscommerce/services/ProductService.java
@@ -9,9 +9,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
-
 @Service
 public class ProductService {
 
@@ -40,6 +37,22 @@ public class ProductService {
         entity.setPrice(dto.getPrice());
 
         entity = productRepository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto){
+
+        Product product = productRepository.findById(id).get();
+        Product entity = product;
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setImgUrl(dto.getImgUrl());
+        entity.setPrice(dto.getPrice());
+
+        entity = productRepository.save(entity);
+
+
         return new ProductDTO(entity);
     }
 }
